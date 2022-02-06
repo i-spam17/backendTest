f2b12dbac03a39ee41d273ec50567b3f02a3ddbe
i-spam <i_spam@bk.ru>
2022-02-06 21:16:20 +0500

    home work 3

--- src/main/java/com/gb/backend/Helpers.java
+++ src/main/java/com/gb/backend/Helpers.java
@@ -1,2 +1,12 @@
-package com.gb.backend;public class Helpers {
+package com.gb.backend;
+
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+import java.io.IOException;
+
+
+public class Helpers {
+    public static String getResourceAsString (String path) throws IOException {
+        return new String(FileUtils.readFileToByteArray(new File(path)));
+    }
 }
--- src/test/java/com/gb/backend/hw3/GetRecipeTest.java
+++ src/test/java/com/gb/backend/hw3/GetRecipeTest.java
@@ -1,4 +1,75 @@
 package com.gb.backend.hw3;
 
-public class GetRecipeTest {
+import com.gb.backend.Helpers;
+import io.restassured.http.ContentType;
+import net.javacrumbs.jsonunit.JsonAssert;
+import net.javacrumbs.jsonunit.core.Option;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.Is.is;
+
+public class GetRecipeTest extends BaseTest {
+    @Test
+    void searchRecipesPositiveTest() throws IOException {
+        responseRecipeInfo
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body(PARAM_NUMBER_KEY, is(PARAM_NUMBER_VALUE));
+
+        String actual = responseRecipeInfo.body().asPrettyString();
+        String expected = Helpers.getResourceAsString(POSITIVE_JSON);
+        JsonAssert.assertJsonEquals(actual, expected, JsonAssert.when(Option.IGNORING_ARRAY_ORDER));
+    }
+
+    @Test
+    void checkRecipeInfo() {
+        given()
+                .param("id", recipeID)
+                .when()
+                .get("/recipes/" + recipeID + "/information")
+                .prettyPeek()
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("id", is(recipeID))
+                .extract()
+                .body()
+                .asPrettyString();
+    }
+
+    @Test
+    void searchRecipeSubstitutionDataTypeTest() {
+        given()
+                .param(PARAM_QUERY_KEY, PARAM_QUERY_VALUE)
+                .param(PARAM_NUMBER_KEY, PARAM_NUMBER_NEGATIVE_VALUE)
+                .when()
+                .get("/recipes/complexSearch")
+                .prettyPeek()
+                .then()
+                .statusCode(404)
+                .contentType(ContentType.HTML);
+    }
+
+    @Test
+    void searchRecipeNegativeTest() throws IOException {
+        String actual = given()
+                .param(PARAM_QUERY_KEY, PARAM_QUERY_NEGATIVE_VALUE)
+                .when()
+                .get("/recipes/complexSearch")
+                .prettyPeek()
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("totalResults", is(0))
+                .extract().response()
+                .asPrettyString();
+
+        String expected = Helpers.getResourceAsString(NEGATIVE_JSON);
+        JsonAssert.assertJsonEquals(actual, expected, JsonAssert.when(Option.IGNORING_ARRAY_ORDER));
+    }
+
 }
